3e69535843a336561acfab3425b8d9df74d3ca2e
Lucas Santos <santoslukz@gmail.com>
2026-02-07 17:36:22 -0300

    feat: add photo and name.

--- android/app/src/main/java/com/polimorfismo/mycard/my_card_estrutura_inicial_flutter/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.polimorfismo.mycard.my_card_estrutura_inicial_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/eu.jpeg'),
+            ),
+            Text(
+              'Lucas Santos',
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Pacifico',
+              ),
+            ),
+          ],
+           
+          ),
+
+        ),
       ),
-    ),
-  );
+
+    );
+  }
 }

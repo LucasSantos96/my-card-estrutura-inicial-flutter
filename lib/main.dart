b449b831fe9bf98fe195e2767d2c920754f95ea4
Lucas Santos <santoslukz@gmail.com>
2026-02-08 13:33:50 -0300

    feat: add cards phone, email.

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,97 @@ class MeuApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
         body: SafeArea(
-          child: Column(
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage('images/eu.jpeg'),
-            ),
-            Text(
-              'Lucas Santos',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Pacifico',
-              ),
-            ),
-          ],
-           
-          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundImage: AssetImage('images/eu.jpeg'),
+                ),
+                Text(
+                  'Lucas Santos',
+                  style: TextStyle(
+                    fontSize: 32,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico',
+                  ),
+                ),
+                Text(
+                  'DESENVOLVEDOR DE SOFTWARE',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.deepOrange.shade100,
+                    //letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Code Pro',
+                  ),
+                ),
 
+                SizedBox(
+                  height: 20,
+                  width: 250,
+                  child: Divider(
+                    color: Colors.deepOrange.shade100,
+                    thickness: 1,
+                  ),
+                ),
+
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+                    child: ListTile(
+                      leading:   Icon(
+                          Icons.phone,
+                          color: Colors.deepOrange,
+                          size: 25,
+                        ),
+                        title:Text(
+                          ' (11) 99999-9999',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.deepOrange,
+                            fontFamily: 'Source Code Pro',
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                    ),
+                  ),
+                ),
+
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+                    child: ListTile(
+                      leading:  Icon(
+                          Icons.email,
+                          color: Colors.deepOrange,
+                          size: 25,
+                        ),
+                        title:
+                         Text(
+                          ' lucas.santos@email.com',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.deepOrange,
+                            fontFamily: 'Source Code Pro',
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-
     );
   }
 }
+
+
+
